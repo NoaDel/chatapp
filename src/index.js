4d56456db84d69916b0292bf27b00d6be606451b
NoaDel <delanonoah@gmail.com>
2020-10-10 15:46:21 -0600

    preston deleted stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from '@testing-library/react'
+// import React from 'react'
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ 
+import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Error from './components/Error';
+import Navigation from './components/Navigation';
+import './Assets/styles.scss';
+
+function success(position) {
+    console.log(position)
+}
+
+
+class App extends Component {
+    render() {
+      return (      
+         <BrowserRouter>
+          <div>
+            <Navigation />
+              <Switch>
+               <Route path="/" component={Home} exact/>
+               {/* <Route path="/about" component={About}/> */}
+               {/* <Route path="/contact" component={Contact}/> */}
+              {/* <Route component={Error}/> */}
+             </Switch>
+          </div> 
+        </BrowserRouter>
+      );
+    }
+  }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    document.querySelector('#root')
+)
